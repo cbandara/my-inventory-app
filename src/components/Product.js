dcbb268aec4225539f632f2ed0d98558f9968ba7
Charutha Bandara <charuthabandara@gmail.com>
2019-12-17 13:31:24 -0500

    edit products button

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Product.css";
+import { Link } from "react-router-dom";
 
 class Product extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class Product extends React.Component {
       <div className="Product">
         <img src={this.props.item.img} alt={this.props.item.id}></img>
         <p>{this.props.item.id}</p>
+        <Link to="/addProduct"><button>Edit</button></Link>
       </div>
     );
   }
