d3c5ba550ef8c8932cfb659ca886103612959634
Charutha Bandara <charuthabandara@gmail.com>
2019-12-16 21:09:10 -0500

    working react form

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,38 +1,38 @@
 import React from "react";
+import { connect } from 'react-redux';
 import Input from './Input';
-import { reduxForm, Field } from 'redux-form';
+// import { reduxForm, Field } from 'redux-form';
 import { addProduct } from "../actions";
 
 class ProductForm extends React.Component {
   onSubmit(event) {
     event.preventDefault();
-
+    console.log(this.id.value)
     const id = this.id.value;
-    const img = this.img;
+    const img = this.img.value;
 
-    this.props.dispatch(addProduct({ id, img }));
-    // this.input.value = '';
-    // this.input.focus();
+    this.props.dispatch(addProduct({ id, img }))
   }
 
   render() {
     return (
+      // Must convert from Redux form to React Form?
       <form onSubmit={e => this.onSubmit(e)}>
         <label htmlFor="id">ID</label>
-        <Field name="id" id="id" type="number" ref="id" component={Input} />
+        <input name="id" id="id" type="number" ref={id => (this.id = id)} required></input>
         <label htmlFor="img">Image</label>
-        <Field name="img" id="img" type="text" component={Input} />
-
+        <input name="img" id="img" type="text" ref={img => (this.img = img)} required></input>
         <button type="submit">Add</button>
       </form>
     );
   }
 }
 
+export default connect()(ProductForm);
 
-export default reduxForm({
-  form: 'product'//is this correct?
-})(ProductForm);
+// export default reduxForm({
+//   form: 'product'//is this correct?
+// })(ProductForm);
 
 // Create React Form
 // Implement action and reducer for add form
