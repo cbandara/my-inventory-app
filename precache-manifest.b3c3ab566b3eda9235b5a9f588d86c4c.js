e64f281c40d697a695ef20aca0f968af16ceafcc
Charutha Bandara <charuthabandara@gmail.com>
2019-12-31 13:39:30 -0500

    Updates

---
+++ precache-manifest.b3c3ab566b3eda9235b5a9f588d86c4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d1c4ae544273992787d63ee4817a9d",
+    "url": "/build/index.html/index.html"
+  },
+  {
+    "revision": "daa182da263166bf263d",
+    "url": "/build/index.html/static/css/main.0984708d.chunk.css"
+  },
+  {
+    "revision": "c41af9d96c04865c8b7b",
+    "url": "/build/index.html/static/js/2.b845cacc.chunk.js"
+  },
+  {
+    "revision": "daa182da263166bf263d",
+    "url": "/build/index.html/static/js/main.97069df6.chunk.js"
+  },
+  {
+    "revision": "42a18f569b3713b7f16a",
+    "url": "/build/index.html/static/js/runtime-main.088f2140.js"
+  }
+]);
